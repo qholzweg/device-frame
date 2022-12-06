00757dd5915baca78f5d09bfae34b3504392f545
qholzweg <qholzweg@gmail.com>
2022-12-06 14:15:25 +0300

    init

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '01589343256e134b4afc');
---
+++ src/save.js
@@ -0,0 +1,6 @@
+import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+
+export default function save( { attributes } ) {
+	const blockProps = useBlockProps.save();
+	return <div { ...blockProps }><InnerBlocks.Content /></div>;
+}
